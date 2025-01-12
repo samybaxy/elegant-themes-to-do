c3a22f0028290f64336d149e0539acb28b715cfa
Samuel Nwaokoro <work@samuelnwaokoro.com>
2025-01-12 22:18:54 +0100

    Refactored TodoList functional component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const TodoList = () => {
+const TodoList = ({ todos, onComplete, onDelete }) => {
+    const renderTodos = () => {
+        return todos.map(( todo, index ) => (
+            <TodoItem key={index} id={index} {...todo} onComplete={onComplete} onDelete={onDelete} />
+        ));
+    };
 
+    return <ul className="list-group todo-list">{renderTodos()}</ul>;
 };
 
 export default TodoList;
