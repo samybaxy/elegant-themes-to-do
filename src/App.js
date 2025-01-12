c9344b1ccab916482e2ef63b89e801e7644aebe0
Samuel Nwaokoro <work@samuelnwaokoro.com>
2025-01-12 21:38:21 +0100

    Refactor life-cycle hooks

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from 'react';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [todos, setTodos] = useState([]);
+    const [title, setTitle] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    // Similar to ComponentDidMount
+    useEffect(() => {
+        const savedTodos = localStorage.getItem( 'et-todos' );
+        if ( savedTodos ) setTodos( JSON.parse( savedTodos ) )
+    }, []);
+
+    // Similar to ComponentDidUpdate
+    useEffect(() => {
+        localStorage.setItem( 'et-todos', JSON.stringify( todos ) )
+    }, [todos]);
 }
 
 export default App;
