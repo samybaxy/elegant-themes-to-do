83e540ea5fc45684ca01eaf34b9dff79ffc727f4
Samuel Nwaokoro <work@samuelnwaokoro.com>
2025-01-12 21:23:11 +0100

    Initialize title and todo states using useState hook

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import TodoList from './components/TodoList';
 
 function App() {
-
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
 }
 
 export default App;
