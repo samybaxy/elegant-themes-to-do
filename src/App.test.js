29d8d9f6ec1d678d5133462505cf0704013ec5ac
Samuel Nwaokoro <work@samuelnwaokoro.com>
2025-01-12 23:34:00 +0100

    tests: added unit tests for completion and deletion

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import {
   render,
   screen,
   fireEvent,
-  getByTestId,
-  queryByTestId,
+  waitFor,
 } from "@testing-library/react";
 import App from "./App";
 
@@ -30,3 +29,65 @@ test("adds new todos", () => {
     todoText
   );
 });
+
+test("marks todos as complete", async () => {
+    render(<App />);
+  
+    // Add a todo
+    const todoText = "Complete the test";
+    fireEvent.change(screen.getByPlaceholderText(/What do you need to do\?/i), {
+      target: { value: todoText },
+    });
+    fireEvent.click(screen.getByText(/\+/i));
+  
+    // Check if todo is in the list
+    const todoItem = await screen.findByText(todoText);
+    expect(todoItem).toBeInTheDocument();
+  
+    // Check the checkbox
+    const checkbox = screen.getByRole('checkbox');
+    fireEvent.click(checkbox);
+  
+    // Wait for the todo to be marked as complete (assuming there's some visual change)
+    await waitFor(() => {
+      expect(checkbox).toBeChecked();
+    });
+  });
+  
+  test("deletes todos", async () => {
+    render(<App />);
+  
+    // Add a todo
+    const todoText = "Delete this test";
+    fireEvent.change(screen.getByPlaceholderText(/What do you need to do\?/i), {
+      target: { value: todoText },
+    });
+    fireEvent.click(screen.getByText(/\+/i));
+  
+    // Check if todo is in the list
+    const todoItem = await screen.findByText(todoText);
+    expect(todoItem).toBeInTheDocument();
+  
+    // Delete the todo
+    const deleteButton = screen.getByText('X');
+    fireEvent.click(deleteButton);
+  
+    // Wait for the todo to be removed from the list
+    await waitFor(() => {
+      expect(screen.queryByText(todoText)).not.toBeInTheDocument();
+    });
+  });
+  
+  // Bug fix test if there was a bug or to ensure functionality
+  test("input field is cleared after adding todo", () => {
+    render(<App />);
+  
+    const todoText = "Clear after add";
+    fireEvent.change(screen.getByPlaceholderText(/What do you need to do\?/i), {
+      target: { value: todoText },
+    });
+    fireEvent.click(screen.getByText(/\+/i));
+  
+    const input = screen.getByPlaceholderText(/What do you need to do\?/i);
+    expect(input.value).toBe("");
+  });
