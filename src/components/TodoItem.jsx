c0cb7e724aab817af19007b6fc6606ea62269c1f
Samuel Nwaokoro <work@samuelnwaokoro.com>
2025-01-12 22:31:20 +0100

    Refactored TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -8,4 +8,27 @@ const TodoItem = ( {id, title, complete, onComplete, onDelete} ) => {
     const handleDelete = () => {
         onDelete( id );
     };
+
+    return (
+        <li className="list-group-item todo-item">
+            <div className="row">
+                <div className="col-2 todo-item__checkbox">
+                    <input 
+                        type="checkbox" 
+                        className="form-control" 
+                        checked={complete}
+                        onChange={handleCompleteCheck}
+                    />
+                </div>
+                <div className="col-8 todo-item__title">
+                    <h3>{title}</h3>
+                </div>
+                <div className="col-2">
+                    <button onClick={handleDelete} className="btn btn-danger">X</button>
+                </div>
+            </div>
+        </li>
+    );
 };
+
+export default TodoItem;
